5fa0f6397bf02c79a7e2b2d48163e0779666b36f
Israel Rodrigues <israelrorigues@gmail.com>
2019-08-25 23:54:30 -0300

    Atualizando projeto.

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,167 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+
+import Notification from '../schemas/Notification';
+
+import Mail from '../../lib/Mail';
+
+class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: {
+        model: User,
+        as: 'provider',
+        attributes: ['id', 'name'],
+        include: [
+          {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+          },
+        ],
+      },
+    });
+    return res.json(appointments);
+  }
+
+  async store(req, res) {
+    const { provider_id, date } = req.body;
+
+    const schema = Yup.object().shape({
+      date: Yup.date().required(),
+      provider_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ message: 'Validation fails' });
+    }
+
+    /**
+     * Verifica se o provider está marcando para ele mesmo
+     */
+    if (provider_id === req.userId) {
+      return res
+        .status(400)
+        .json({ message: 'You can not appoint for yourself.' });
+    }
+
+    /**
+     * Check if provider exists
+     */
+
+    const isProvider = await User.findOne({
+      where: {
+        id: provider_id,
+        provider: true,
+      },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ message: 'You can only create appointments with providers' });
+    }
+
+    /**
+     * Check past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ message: 'Past date is not permitted' });
+    }
+
+    /**
+     * Check appointment is available
+     */
+    const isAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (isAvailability) {
+      return res
+        .status(400)
+        .json({ message: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    /**
+     * Notify
+     */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
+    });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointment 2 hours in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name}<${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '574ca432e223ab',
+    pass: 'b1eeac1ff44e82',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
